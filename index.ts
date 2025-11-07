a7db0f230b47f665a2260618b262b73078825968
SlayHand <fredym6ttus@gmail.com>
2025-11-07 13:52:54 +0200

    mongo ühendus on olemas, tegelen prg selle osaga article.ts juhendis

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import stringsController from "./controllers/strings";
 import cors = require("cors");
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 
-// Pane CORS MIDDLEWARE ENNE route'e
 app.use(
   cors({
     origin: ["http://localhost:3006"],
-    // kui vajad küpsiseid/Authorizationi:
     // credentials: true,
   })
 );
@@ -17,8 +19,27 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  throw new Error("MONGO_URI is not defined in .env");
+}
+
+mongoose.connect(mongoUri);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+// sinu enda controllerid
 app.use("/", stringsController);
 
-app.listen(3000, () => {
-  console.log(`[server]: Server is running at http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
